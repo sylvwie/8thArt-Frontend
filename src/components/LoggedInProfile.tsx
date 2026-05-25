3c0d86222c0c2a6cd3aee02f36efdfb185dc06fb
sylvvww <s.penati@ied.edu>
2026-05-26 01:48:11 +0200

    profile boxes updated

--- src/components/LoggedInProfile.tsx
+++ src/components/LoggedInProfile.tsx
@@ -1,21 +1,23 @@
-import profilePicture from "/src/assets/template/profileplaceholder.gif";
+import profilePicture from "/src/assets/template/propic-template.webp";
 import bookmarkIcon from "/src/assets/icons/bookmark.svg";
 import cartIcon from "/src/assets/icons/cart.svg";
 
 export function LoggedInProfileBox() {
 	return <>
 			<div className="HoverProfileBoxWrapper">
-				<div className="HoverProfileBoxHeader">
-					<span>~ Profile</span>
-					<span className="loginStatusText">LOGGED IN</span>
-				</div>
-				<div className="HoverProfileBoxContent">
-					<div className="HoverProfilePropicContainer">
-						<img className="profilepicture" src={profilePicture} alt="Profile Picture" />
+				<div className="HoverProfileBoxContainer">
+					<div className="HoverProfileBoxHeader">
+						<span className="profile-text-box-header">~ Profile</span>
+						<span id="loggedStatusText">LOGGED IN</span>
 					</div>
-					<div className="profileBoxSectionsContainer">
-						<button> <img src={bookmarkIcon} alt="Bookmark Icon" /> Saves</button>
-						<button> <img src={cartIcon} alt="Cart Icon" /> Cart</button>
+					<div className="HoverProfileBoxContent">
+						<div className="HoverProfilePropicContainer">
+							<img className="profilepicture" src={profilePicture} alt="Profile Picture" />
+						</div>
+						<div id="loggedProfileBoxSectionsContainer">
+							<button> <img src={bookmarkIcon} alt="Bookmark Icon" /> Saves </button>
+							<button> <img src={cartIcon} alt="Cart Icon" /> Cart </button>
+						</div>
 					</div>
 				</div>
 			</div>
--- src/components/NotLoggedInProfile.tsx
+++ src/components/NotLoggedInProfile.tsx
@@ -1,18 +1,23 @@
+import { useNavigate } from "react-router-dom";
 import profileplaceholder from "/src/assets/template/profileplaceholder.gif";
 
 export function NotLoggedInProfileBox() {
+	  const navigate = useNavigate();
 	return <>
 			<div className="HoverProfileBoxWrapper">
-				<div className="HoverProfileBoxHeader">
-					<span>~ Profile</span>
-					<span className="loginStatusText">NOT LOGGED</span>
-				</div>
-				<div className="HoverProfileBoxContent">
-					<div className="HoverProfilePropicContainer">
-						<img className="profilepicture" src={profileplaceholder} alt="Profile Picture" />
+				<div className="HoverProfileBoxContainer">
+					<div className="HoverProfileBoxHeader">
+						<span className="profile-text-box-header">~ Profile</span>
+						<span id="notLoggedStatusText">NOT LOGGED</span>
 					</div>
-					<div className="profileBoxSectionsContainer">
-						<button>Register or Login to access the profile section</button>
+					<div className="HoverProfileBoxContent">
+						<div className="HoverProfilePropicContainer">
+							<button className="login-redirect-link-button" onClick={() => navigate("/login")}>
+								Login to access the profile section
+							</button>
+							<img className="profilepicture" src={profileplaceholder} alt="Profile Picture" />
+						</div>
+						<div id="notLoggedProfileBoxSectionsContainer"></div>
 					</div>
 				</div>
 			</div>
